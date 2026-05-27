5841db74f92520ea35e456996f0220e1cc0a0977
Oliver Stolpe <oliver.stolpe@charite.de>
2026-05-27 09:07:13 +0200

    fix: loosening min cov thres; pylance and pyrefly complains, added tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ import pytest
 import requests
 import responses as rsps
 
-from cbio_ingest.api import ApiSession
+from cbio_ingest.api import ApiSession, sanity_check_server
 
 
 @pytest.fixture
@@ -49,3 +49,33 @@ def test_timeout_raises_click_exception(session: ApiSession) -> None:
     rsps.add(rsps.GET, "http://testserver/", body=requests.Timeout())
     with pytest.raises(click.ClickException, match="timed out"):
         session.get("http://testserver/")
+
+
+def make_fake_ctx(url="http://testserver", token="t"):
+    ctx = click.Context(click.Command("dummy"))
+    ctx.obj = {"url": url, "token": token}
+    return ctx
+
+
+@rsps.activate
+def test_sanity_check_server_valid(monkeypatch):
+    ctx = make_fake_ctx()
+    rsps.add(rsps.GET, "http://testserver/", json={"title": "cBioPortal Ingest API"}, status=200)
+    # Should not raise
+    sanity_check_server(ctx)
+
+
+@rsps.activate
+def test_sanity_check_server_invalid_title(monkeypatch):
+    ctx = make_fake_ctx()
+    rsps.add(rsps.GET, "http://testserver/", json={"title": "Not cBioPortal"}, status=200)
+    with pytest.raises(click.ClickException, match="does not seem to be the cbio-ingest API"):
+        sanity_check_server(ctx)
+
+
+@rsps.activate
+def test_sanity_check_server_malformed_json(monkeypatch):
+    ctx = make_fake_ctx()
+    rsps.add(rsps.GET, "http://testserver/", body="not json", status=200)
+    with pytest.raises(click.ClickException, match="does not seem to be the cbio-ingest API"):
+        sanity_check_server(ctx)
